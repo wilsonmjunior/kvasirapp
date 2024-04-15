61977e028cc273972c453fe4b0a91f5aec3117e1
Wilson M Junior <wilsonmjuniorx@gmail.com>
2024-04-14 22:35:12 -0300

    feat: add react native gesture handler

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useFonts } from "expo-font";
 
 import { Routes } from './src/routes';
@@ -13,17 +16,14 @@ export default function App() {
     return <Loading />
   }
 
-  console.warn('Font loading: ',fontsLoaded, fontError);
-  
-
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Routes />
-    </>
+    </GestureHandlerRootView>
   );
 }
